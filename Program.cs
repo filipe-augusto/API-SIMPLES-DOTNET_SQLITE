bd4dacd082ca8c106db782d5d86da5312fe0ce02
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2023-02-17 08:59:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+//dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+//dotnet add package Microsoft.EntityFrameworkCore.Design
+//dotnet add package Microsoft.EntityFrameworkCore.Sqlite.Design
+//Microsoft.EnityFrameworkCore.Sqlite.Tools
+//dotnet ef migrations add CreateDatabase
